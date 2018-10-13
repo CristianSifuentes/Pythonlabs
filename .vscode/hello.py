a7c488ee0b438fc5f6e9aec30e33f1dc39b23dd3
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2018-10-13 17:06:43 -0500

    CSC 2018/10/13: Comentarios, Operaciones aritmética y Variables

--- .vscode/hello.py
+++ .vscode/hello.py
@@ -1,1 +1,16 @@
-print("Hola mundo")
+""" Este es un ejemplo de comentarios """
+
+""" Operaciones aritmética  """
+print(2 + 10)
+print(2 - 10)
+print(2 * 10)
+print(2 / 10)
+
+""" Variables  """
+counter = 100          # An integer assignment
+miles   = 1000.0       # A floating point
+name    = "John"       # A string
+
+print(counter)
+print(miles)
+print(name)
