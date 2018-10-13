1e7c6a7f68decad66b52fb2f90ff7a434fa811d7
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2018-10-13 17:30:37 -0500

    CSC 2018/10/13: Multiples Asignaciones y Cadenas

--- .vscode/hello.py
+++ .vscode/hello.py
@@ -5,6 +5,8 @@ print(2 + 10)
 print(2 - 10)
 print(2 * 10)
 print(2 / 10)
+print(100 // 10) # redondeo
+print(2 ** 5)  # doble potencia
 
 """ Variables  """
 counter = 100          # An integer assignment
@@ -14,3 +16,25 @@ name    = "John"       # A string
 print(counter)
 print(miles)
 print(name)
+
+""" Multiples Asignaciones  """
+a = b = c = 100
+print(a)
+print(b)
+print(c)
+
+d,e,f = 1,2,"john"
+print(d)
+print(e)
+print(f)
+
+""" Cadenas  """
+
+str = 'Hello World!'
+
+print(str )         # Prints complete string
+print(str[0])      # Prints first character of the string
+print(str[2:5])    # Prints characters starting from 3rd to 5th
+print(str[2:])    # Prints string starting from 3rd character
+print(str * 2)   # Prints string two times
+print(str + "TEST") # Prints concatenated string
