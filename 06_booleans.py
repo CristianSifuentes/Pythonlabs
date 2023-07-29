ae38247b44e0dca5d744628d8c09ffd4f6f1338f
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2023-07-29 13:19:46 -0600

    Creating boolean examples

---
+++ 06_booleans.py
@@ -0,0 +1,20 @@
+is_single = True
+print(is_single)
+print(type(is_single))
+
+print(not True)
+print(not False)
+
+is_single = not is_single
+print(is_single)
+
+print(bool(True), bool(False), bool(None))
+print(bool(0), bool(-1), bool('text'), bool(24))
+
+print(bool(0.0), bool(0.j))
+print(True+True)
+print(False+False)
+print(True+False)
+print(False+True)
+print(not (False+True))
+
