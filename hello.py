fa95897f8eddf3a1e75b1d437d4115774a8c5cbc
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2023-07-29 12:01:04 -0600

    Adding py files for practice

---
+++ hello.py
@@ -0,0 +1,40 @@
+""" Este es un ejemplo de comentarios """
+
+""" Operaciones aritmética  """
+print(2 + 10)
+print(2 - 10)
+print(2 * 10)
+print(2 / 10)
+print(100 // 10) # redondeo
+print(2 ** 5)  # doble potencia
+
+""" Variables  """
+counter = 100          # An integer assignment
+miles   = 1000.0       # A floating point
+name    = "John"       # A string
+
+print(counter)
+print(miles)
+print(name)
+
+""" Multiples Asignaciones  """
+a = b = c = 100
+print(a)
+print(b)
+print(c)
+
+d,e,f = 1,2,"john"
+print(d)
+print(e)
+print(f)
+
+""" Cadenas  """
+
+str = 'Hello World!'
+
+print(str )         # Prints complete string
+print(str[0])      # Prints first character of the string
+print(str[2:5])    # Prints characters starting from 3rd to 5th
+print(str[2:])    # Prints string starting from 3rd character
+print(str * 2)   # Prints string two times
+print(str + "TEST") # Prints concatenated string
