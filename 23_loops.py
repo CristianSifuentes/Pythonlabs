ea4a8d086a4808dcafd66c5ee8a36aef8c630116
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2023-08-02 15:22:58 -0600

    Example using loops

---
+++ 23_loops.py
@@ -0,0 +1,45 @@
+nestedlist = [[2,4,6,8,10],[1,3,5,7,9]]
+
+print("Accessing the third element of the second list", nestedlist[1][2])
+'''
+0 => [2,4,6,8,10],
+1 => [1,3,5,7,9]
+
+nestedlist[1][2]
+
+[
+    1, => 0
+    3, => 1
+    5, => 2 (selected)
+    7, => 3 
+    9  => 4
+]
+
+'''
+for i in nestedlist:
+    print("list", i, "elements")
+
+'''
+list [2, 4, 6, 8, 10] elements
+list [1, 3, 5, 7, 9] elements
+'''
+
+for i in nestedlist:
+    print("list", i, "elements")
+    for j in i:
+        print(j)
+
+'''
+list [2, 4, 6, 8, 10] elements
+2
+4
+6
+8
+10
+list [1, 3, 5, 7, 9] elements
+1
+3
+5
+7
+9
+'''
