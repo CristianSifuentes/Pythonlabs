c6343c331a7c89a5806ca3f88fd8e5fe1721f15d
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2023-08-02 13:08:17 -0600

    Creating examples using dict

--- 20_dict.py
+++ 20_dict.py
@@ -1,9 +1,50 @@
-person = {"name": "Jessa", 'country': "USA", "telephone": 1178}
+person = {"name": "Jessa", 'country': "USA", "telephone": 1178, "lang": ['python', 'javascript']}
 
 # update dictionary by adding 2 new keys
 person["weight"] = 50
 person.update({"height": 6})
 
+# pass new keys as as list of tuple
+person.update([("city","Texas"), ("company", "Google")])
+print(person) # {'name': 'Jessa', 'country': 'USA', 'telephone': 1178, 'lang': ['python', 'javascript'], 'weight': 50, 'height': 6, 'city': 'Texas', 'company': 'Google'}
+
 # print the updated dictionary
-print(person)
-# output {'name': 'Jessa', 'country': 'USA', 'telephone': 1178, 'weight': 50, 'height': 6}
+print(person) #  {'name': 'Jessa', 'country': 'USA', 'telephone': 1178, 'weight': 50, 'height': 6}
+
+person["height"] -= 2
+print(person) # {'name': 'Jessa', 'country': 'USA', 'telephone': 1178, 'weight': 50, 'height': 4}
+
+person["lang"].append(' Robert')
+print(person) 
+# {'name': 'Jessa', 'country': 'USA', 'telephone': 1178, 'lang': ['python', 'javascript', ' Robert'], 'weight': 50, 'height': 4}
+
+del person['country']
+print(person) # {'name': 'Jessa', 'telephone': 1178, 'lang': ['python', 'javascript', ' Robert'], 'weight': 50, 'height': 4}
+
+person_details = {"name": "Jessa", "country": "USA", "telephone": 1178}
+
+# set default value if key doesn't exists
+person_details.setdefault('state', 'Texas')
+print(person_details)# {'name': 'Jessa', 'country': 'USA', 'telephone': 1178, 'state': 'Texas'}
+
+# key doesn't and value not mentioned.default None
+person_details.setdefault("zip")
+print(person_details)#{'name': 'Jessa', 'country': 'USA', 'telephone': 1178, 'state': 'Texas', 'zip': None}
+
+# key exists and value mentioned. doesn't  change value
+person_details.setdefault('country', 'Canada')
+print(person_details)# {'name': 'Jessa', 'country': 'USA', 'telephone': 1178, 'state': 'Texas', 'zip': None}
+
+#person_details.pop()
+#print(person_details) # TypeError: pop expected at least 1 argument, got 0
+
+person_details.pop('name')
+print(person_details) # {'country': 'USA', 'telephone': 1178, 'state': 'Texas', 'zip': None}
+
+print(person_details.items())
+# dict_items([('country', 'USA'), ('telephone', 1178), ('state', 'Texas'), ('zip', None)])
+
+print(person_details.keys()) 
+# dict_keys(['country', 'telephone', 'state', 'zip'])
+
+print(person_details.values()) # dict_values(['USA', 1178, 'Texas', None])
---
+++ dict.py
@@ -0,0 +1,29 @@
+person = {
+    'name': 'Nicolas',
+    'lastName': 'Molina',
+    'age': 29
+}
+
+'''
+Agregar un nuevo elemento al diccionario con la llave "twitter" y el valor "@nicobytes".
+Actualizar el valor del elemento con la llave "name" con el valor "Felipe".
+Eliminar el elemento del diccionario con la llave "age".
+Imprimir una lista con las llaves del diccionario.
+Imprimir una lista con los valores del diccionario.
+
+'''
+person["twitter"] = '@nicobytes'
+print(person)
+
+person["name"] = 'Felipe'
+print(person) # {'name': 'Felipe', 'lastName': 'Molina', 'age': 29, 'twitter': '@nicobytes'}
+
+del person['age']
+print(person) # {'name': 'Felipe', 'lastName': 'Molina', 'twitter': '@nicobytes'}
+
+print(person.keys()) # dict_keys(['name', 'lastName', 'twitter'])
+
+print(person.values()) # dict_values(['Felipe', 'Molina', '@nicobytes'])
+
+
+
