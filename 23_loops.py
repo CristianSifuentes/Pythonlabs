d3f5a6cbdcfc0e07fefbd4d20a257ab7bdcebef9
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2023-08-08 13:29:38 -0600

    Examples using nested list

--- 23_loops.py
+++ 23_loops.py
@@ -43,3 +43,13 @@ list [1, 3, 5, 7, 9] elements
 7
 9
 '''
+
+
+# outer loop
+for i in range(1, 11):
+    # nested loop
+    # to iterate from 1 to 10
+    for j in range(1, 11):
+        # print multiplication
+        print(i * j, end=' ')
+    print()
