1b914ad4b544e52ebdf7104c7b736c4a4a1ea015
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2023-08-09 14:50:17 -0600

    Creating while advanced examples

--- 21_while.py
+++ 21_while.py
@@ -13,3 +13,35 @@ while number > 10:
     count = count + 1
 
 print('Total iteration required', count)
+
+
+i = 1
+while i <= 5:
+    j = 1
+    while j <= 10:
+        print(j, end='')
+        j = j + 1
+    i = i + 1
+    print()    
+
+'''
+12345678910
+12345678910
+12345678910
+12345678910
+12345678910
+'''
+
+# for loop inside While loop
+print('Show perfect number from 1 to 100')
+n = 2
+# outer while loop
+while n <= 100:
+    x_sum = 0
+    # inner for loop
+    for i in range(1, n):
+        if n % i == 0:
+            x_sum += i
+    if x_sum == n:
+        print('Perfect number:', n)
+    n += 1
