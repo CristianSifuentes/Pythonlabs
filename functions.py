9afede5e07aca14913e1edfc98d41c1d0374a06f
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2023-08-18 13:12:29 -0600

    Creating function for calculate addition

--- functions.py
+++ functions.py
@@ -14,3 +14,17 @@ def course_func(name, course_name):
 
 # call function
 course_func('John', 'Python')
+
+
+# function
+def calculator(a, b):
+    add = a + b
+    # return the addition
+    return add
+
+# call function
+# take return value in variable
+res = calculator(20, 5)
+
+print("Addition :", res)
+# Output Addition : 25
