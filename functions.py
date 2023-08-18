1857be6e7d062bf5e1e7af158b5a86b468ad4ec4
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2023-08-18 13:03:11 -0600

    Function in Python

---
+++ functions.py
@@ -0,0 +1,16 @@
+# function
+def message():
+    print("Welcome to PYnative")
+
+# call function using its name
+message()
+
+
+
+# function
+def course_func(name, course_name):
+    print("Hello", name, "Welcome to PYnative")
+    print("Your course name is", course_name)
+
+# call function
+course_func('John', 'Python')
