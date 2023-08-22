9ea2eac73d4c68fc151ef2ff0bf1ecd05256fec6
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2023-08-22 12:31:30 -0600

    Examples using function

--- functions.py
+++ functions.py
@@ -28,3 +28,36 @@ res = calculator(20, 5)
 
 print("Addition :", res)
 # Output Addition : 25
+
+
+
+# function
+def even_odd(n):
+    # check numne ris even or odd
+    if n % 2 == 0:
+        print('Even number')
+    else:
+        print('Odd Number')
+
+# calling function by its name
+even_odd(19)
+# Output Odd Number
+
+
+
+# import randint function
+from random import randint
+
+# call randint function to get random number
+print(randint(10, 20))
+# Output 14
+
+
+# Single-Line Docstring
+
+def factorial(x):
+    """This function returns the factorial of a given number."""
+    return x
+
+# access doc string
+print(factorial.__doc__)
