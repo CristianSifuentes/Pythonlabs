32cc3f8675eb1dbf38455ee6e30895c1710b5118
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2023-08-23 13:25:00 -0600

    Examples using functions

--- functions.py
+++ functions.py
@@ -61,3 +61,33 @@ def factorial(x):
 
 # access doc string
 print(factorial.__doc__)
+
+print(help(factorial))
+
+# Multi-Line Docstring
+
+def any_fun(parameter1):
+    """              
+    Description of function
+                    
+    Arguments:   
+    parameter1(int):Description of parameter1
+                    
+    Returns:      
+    int value     
+    """              
+print(any_fun.__doc__)
+
+# Return Value From a Function
+
+def is_even(list1):
+    even_num = []
+    for n in list1:
+        if n % 2 == 0:
+            even_num.append(n)
+    # return a list
+    return even_num
+
+# Pass list to the function
+even_num = is_even([2, 3, 42, 51, 62, 70, 5, 9])
+print("Even numbers are:", even_num)
